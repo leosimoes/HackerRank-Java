dbc9be7f56b9ccf4989ea763081d7df8236cf9f7
Leonardo <leosimoes.engcomp@gmail.com>
2023-06-14 10:28:18 -0300

    HackerRank - Java - Introduction - Welcome to Java

---
+++ Introduction/Welcome-to-Java/Welcome-to-Java.java
@@ -0,0 +1,14 @@
+/*
+    Author: Leonardo Simoes
+    Date: 06/14/2023
+    HackerRank - Java - Introduction - Welcome to Java!
+    Language: Java 8
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
