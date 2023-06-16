249cccb0a7d799046afe828934d0bf739938cc5e
Leonardo <leosimoes.engcomp@gmail.com>
2023-06-16 16:14:44 -0300

    HackerRank - Java - Introduction - Java Stdin and Stdout I

---
+++ Introduction/Java-Stdin-and-Stdout-I/Java-Stdin-and-Stdout-I.java
@@ -0,0 +1,23 @@
+/*
+    Author: Leonardo Simoes
+    Date: 06/16/2023
+    HackerRank - Java - Introduction - Java Stdin and Stdout I
+    Language: Java 8
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
